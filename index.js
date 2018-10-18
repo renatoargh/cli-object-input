912d03fb9268bca6a231361e0d1ba94c9c6d133f
Renato Gama <renatoargh@gmail.com>
2018-10-18 11:24:00 +0200

    More basic validators

--- index.js
+++ index.js
@@ -2,86 +2,92 @@ const Ui = require('./lib/ui')
 
 const ui = new Ui({
   "type": "object",
-  "required": [
-    "name",
-    "parentId"
-  ],
+  // "required": [
+  //   "name",
+  //   "parentId"
+  // ],
   "properties": {
-    "name": {
-      "type": "string",
-      "minLength": 1,
-      "maxLength": 5,
-      "nullable": true,
-      "pattern": "^asd$",
-      "description": "Name of the group",
-      "example": "my_group",
-      // "enum": ["bsb", "sp", "bh"]
-    },
-    "secret_pass": {
-      "type": "string",
-      "format": "password",
-      "minLength": 3
-    },
-    "someBytes": {
-      "type": "string",
-      "format": "byte"
-    },
-    "a_nice_boolean_property": {
-      "type": "boolean",
-      "description": "Name of the group"
-    },
-    "parentId": {
-      "type": "string",
-      "format": "uuid",
-      "pattern": "^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$",
-      "description": "ID of the parent group as a string",
-      "example": "dc952b4a-5b3d-4ff9-8431-796c77643cc9"
-    },
-    "deviceIds": {
-      "type": "array",
-      "items": {
-        "type": "object",
-        "required": [
-          "name",
-          "parentId"
-        ],
-        "properties": {
-          "name": {
-            type: 'string',
-            example: 'sbrubbles'
-          },
-          "age": {
-            type: 'number'
-          }
-        },
-      },
-      "description": "IDs for the devices belonging to the group"
-    },
-    "groups": {
-      type: "array",
-      items: {
-        type: "object",
-        "properties": {
-          "sbrubbles": {
-            "type": "string",
-            "example": "asdasd",
-            "default": "lololo"
-          },
-          "arrayDeString": {
-            type: "array",
-            items: {
-              "type": "string",
-              example: "alalao"
-            } 
-          }
-        }
-      }
-    },
+    "age": {
+      type: 'number',
+      minimum: 5,
+      maximum: 20,
+      multipleOf: 2
+    }
+    // "name": {
+    //   "type": "string",
+    //   "minLength": 1,
+    //   "maxLength": 5,
+    //   "nullable": true,
+    //   "pattern": "^asd$",
+    //   "description": "Name of the group",
+    //   "example": "my_group",
+    //   // "enum": ["bsb", "sp", "bh"]
+    // },
+    // "secret_pass": {
+    //   "type": "string",
+    //   "format": "password",
+    //   "minLength": 3
+    // },
+    // "someBytes": {
+    //   "type": "string",
+    //   "format": "byte"
+    // },
+    // "a_nice_boolean_property": {
+    //   "type": "boolean",
+    //   "description": "Name of the group"
+    // },
+    // "parentId": {
+    //   "type": "string",
+    //   "format": "uuid",
+    //   "pattern": "^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$",
+    //   "description": "ID of the parent group as a string",
+    //   "example": "dc952b4a-5b3d-4ff9-8431-796c77643cc9"
+    // },
+    // "deviceIds": {
+    //   "type": "array",
+    //   "items": {
+    //     "type": "object",
+    //     "required": [
+    //       "name",
+    //       "parentId"
+    //     ],
+    //     "properties": {
+    //       "name": {
+    //         type: 'string',
+    //         example: 'sbrubbles'
+    //       },
+    //       "age": {
+    //         type: 'number'
+    //       }
+    //     },
+    //   },
+    //   "description": "IDs for the devices belonging to the group"
+    // },
+    // "groups": {
+    //   type: "array",
+    //   items: {
+    //     type: "object",
+    //     "properties": {
+    //       "sbrubbles": {
+    //         "type": "string",
+    //         "example": "asdasd",
+    //         "default": "lololo"
+    //       },
+    //       "arrayDeString": {
+    //         type: "array",
+    //         items: {
+    //           "type": "string",
+    //           example: "alalao"
+    //         } 
+    //       }
+    //     }
+    //   }
+    // },
   }
 })
 
 async function main () {
-  const results = await ui.getValues()
+  const results = await ui.getUserInput()
   console.log(JSON.stringify(results, null, 2))
 }
 
